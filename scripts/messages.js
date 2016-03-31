e6c78c193d4bde5c746b1b2d6c426305dd4b264c
François Crossette <francois.crossette@gmail.com>
2016-03-31 17:58:42 +0200

    Added stuff

--- scripts/messages.js
+++ scripts/messages.js
@@ -2,5 +2,7 @@ var messages = [
     "Mirakl radio",
     "Satisfy and empower clients",
     "Go above and beyond",
-    "Never say it's not my job"
+    "We never say it's not my job",
+    "Your changes have been saved",
+    "Your offer for product iPad has been added"
 ];
